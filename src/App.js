754bd3ae7bb357da6f8aa982c8e5a75b9749996d
shindeAkshu <sakshata416@gmail.com>
2022-08-01 09:55:24 -0700

    Contact_List

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import './App.css';
 
 const App = ()=> {
-  const title ="Hello React";
-  return (
-    <div>
-    <h1>{title}</h1>  
-    <h2 onClick={() => alert("You clicked the message")}>This is my first react component</h2>
-    </div>
-    
-  );
+ 
+  return 
 }
 
 export default App;
---
+++ src/ContactCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contactcard = () => {
+<div className='contact-card'>
+      <img src="https://via.placeholder.com/150" alt='profile'></img>
+      <div className='users-details'>
+        <p>Name:akshta</p>
+        <p>Email:sakshata@gmailcom</p>
+        <age>Age:25</age>
+      </div>
+    
+    </div>
+}
+
+
+
+
+export default Contactcard;
