bbdf4ae65c6f48ad195e47c1cb51b3409d3810d6
shindeAkshu <sakshata416@gmail.com>
2022-08-01 22:50:50 -0700

    Contact_List

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 import Contactcard from './ContactCard';
 
 const App = ()=> {
- 
+   const contacts = [
+    {avtarUrl:"https://via.placeholder.com/150",
+    name:"Akshata Shinde",
+    email:"fdghjkl@gmail.com",
+    age: 19
+    },
+    {avtarUrl:"https://via.placeholder.com/150",
+    name:"Jhon Ken",
+    email:"fdghjkl@gmail.com",
+    age: 25
+    },
+    {avtarUrl:"https://via.placeholder.com/150",
+    name:"Manish Pol",
+    email:"fdghjkl@gmail.com",
+    age: 20
+    }
+   ]
   return(
     <div>
       <Contactcard 
