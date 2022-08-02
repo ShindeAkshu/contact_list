e0437d6dc6d5248c340bca8227a35c03b2e9dada
shindeAkshu <sakshata416@gmail.com>
2022-08-01 22:49:39 -0700

    Contact_List

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useState } from "react";
-const Contactcard = () => {
-    const [showAge,setShowAge] =useState(true);
+
+const Contactcard = (props) => {
+
+    const [showAge,setShowAge] =useState(false);
 <div className='contact-card'>
-      <img src="https://via.placeholder.com/150" alt='profile'></img>
+      <img src={props.avtarUrl} alt='profile'></img>
       <div className='users-details'>
-        <p>Name:akshta</p>
-        <p>Email:sakshata@gmailcom</p>
-        <button>Show Age</button>
-        {showAge && <p>Age:25</p> }
+        <p>Name:{props.name}</p>
+        <p>Email:{props.email}</p>
+        <button onClick={() =>setShowAge(!showAge)}>Show Age</button>
+        {showAge && <p>Age:{props.age}</p> }
       </div>
     
     </div>
