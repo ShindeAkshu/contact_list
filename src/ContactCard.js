adda6909b8d68e345ebaa189ca45ed18818f9b28
shindeAkshu <sakshata416@gmail.com>
2022-08-04 11:07:03 -0700

    Contact-List

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -16,6 +16,21 @@ const Contactcard = (props) => {
     </div>
 }
 
+const Contact = (props) => {
+
+  const [showAge,setShowAge] =useState(false);
+<div className='contact-card'>
+    <img src={props.avtarUrl} alt='profile'></img>
+    <div className='users-details'>
+      <p>Name:{props.name}</p>
+      <p>Email:{props.email}</p>
+      <button onClick={() =>setShowAge(!showAge)}>Show Age</button>
+      {showAge && <p>Age:{props.age}</p> }
+    </div>
+  
+  </div>
+}
+
 
 
 
