4d42b7afdffccb0436f1240ba745c96dd6580250
shindeAkshu <sakshata416@gmail.com>
2022-08-05 13:31:49 -0700

    Contact-List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import Contactcard from './ContactCard';
+import { useState } from 'react';
 
 const App = ()=> {
-   const contacts = [
+  /* const contacts = [
     {avtarUrl:"https://via.placeholder.com/150",
     name:"Akshata Shinde",
     email:"fdghjkl@gmail.com",
@@ -20,12 +21,18 @@ const App = ()=> {
     email:"fdghjkl@gmail.com",
     age: 20
     }
-   ]
+   ];*/
+   
+   const [contacts , setContacts] = useState([]);
+   fetch("https://randomuser.me/api/?results=5")
+   .then(response => response.json()) 
+   .then(data => console.log(data));
+
   return(
     <div>
       {contacts.map((contact,index)=>{
         return(
-        <Contactcard 
+        <Contactcard key={index}
         avtarUrl= {contact.avtarUrl}
         name={contact.name}
         email={contact.email}
